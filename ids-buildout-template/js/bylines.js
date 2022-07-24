65602f407fce316fdc01ff6e23fa8a29c1d080be
Carson TerBush <carson.terbush@gmail.com>
2022-07-24 16:57:02 -0400

    add bio generation

--- ids-buildout-template/js/bylines.js
+++ ids-buildout-template/js/bylines.js
@@ -1,33 +1,38 @@
 // TODO: replace bylines based on metadata
 'use strict';
 
-const byline_types = ["Written by", "Photos by", "Design and development by", "Graphics by"]
+const byline_types = ["By", "Photos by", "Design and development by", "Graphics by"]
 let bylines_html = '';
+let bios_html = '';
 fetch("../metadata.json")
     .then(response => response.json())
     .then(data => {
         for (let type of byline_types) {
             if (data.bylines[type]) {
 
-                bylines_html += `<p>${type} <a href="idsnews.com/staff/${data.bylines[type].name.split(' ').join('-')}">${data.bylines[type].name}</a></p>`;
-                if (data.bylines[type].email) {
-                    bylines_html += `<p><small><a href="mailto:${data.bylines[type].email}" target="_blank">${data.bylines[type].email}</a>`;
-                } else {
-                    console.log(`There is no email listed for ${data.bylines[type].name} in metadata.json. Please update!!`)
-                }
-                if (!data.bylines[type].email) {
-                    bylines_html += `<p><small> <span style="color:red;">EMAIL MISSING`;
-                }
-                if (data.bylines[type].twitter) {
-                    bylines_html += ` | <a
-                            href="https://twitter.com/${data.bylines[type].twitter}" target="_blank">@${data.bylines[type].twitter}</a></small>
-                </p>`;
-                } else if (!data.bylines[type].email && !data.bylines[type].twitter) {
-                    bylines_html += '</p>';
+                bylines_html += `<p>${type} <a href="https://idsnews.com/staff/${data.bylines[type].name.split(' ').join('-')}">${data.bylines[type].name}</a></p>`;
+
+                // if all the right data is available, generate author bio at bottom of article
+                if (data.bylines[type].twitter && data.bylines[type].pfp && data.bylines[type].bio) {
+                    bios_html += `<div class="bio">
+                        <div>
+                            <img src="${data.bylines[type].pfp}"
+                                alt="${data.bylines[type].name}">
+                            <div>
+                                <p>${type} <a href="https://idsnews.com/staff/${data.bylines[type].name.split(' ').join('-')}" target="_blank">${data.bylines[type].name}</a></p>
+                                <p>${data.bylines[type].bio}  <span><a
+                                href="mailto:${data.bylines[type].email}" target="_blank"><i
+                                    class="fa fa-envelope"></i> Email</a></span> <span><a
+                                            href="https://twitter.com/${data.bylines[type].twitter}" target="_blank"><i
+                                                class="fab fa-twitter"></i> Twitter</a></span></p>
+                            </div>
+                        </div>
+                    </div>`
                 }
             }
         }
 
         document.querySelector('#bylines').innerHTML = bylines_html;
+        document.querySelector('.author-bios').innerHTML = bios_html;
     })
     .catch(err => console.log(err))
