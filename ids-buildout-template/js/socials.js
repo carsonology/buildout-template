47bb853a2522309bf3febf4f9d60bb1dcd711013
Carson TerBush <carson.terbush@gmail.com>
2022-07-24 16:57:23 -0400

    auto populate meta social tags

--- ids-buildout-template/js/socials.js
+++ ids-buildout-template/js/socials.js
@@ -1,5 +1,7 @@
-// TODO: update social buttons (nav) and footer with proper social links
+// update social buttons (nav) and meta tags with proper social links
 
+let meta_twitter = document.querySelectorAll('meta[name*="twitter"]');
+let meta_og = document.querySelectorAll('meta[property*="og"]');
 fetch("../metadata.json")
     .then(response => response.json())
     .then(data => {
@@ -12,5 +14,15 @@ fetch("../metadata.json")
         <a href="${twitter}" target="_blank"><i class="fab fa-twitter"></i></a>
         <a href="${reddit}" target="_blank"><i class="fab fa-reddit"></i></a>
         `;
+
+        meta_og[0].content = `https://specials.idsnews.com/${data.url}`;
+        meta_og[2].content = data.headline;
+        meta_og[3].content = data.abstract;
+        meta_og[5].content = data.dom_image;
+
+        meta_twitter[3].content = data.headline;
+        meta_twitter[4].content = data.abstract;
+        meta_twitter[5].content = data.dom_image;
+
     })
     .catch(err => console.log(err))
